bbbb5e0d5057b581902a382c596ff30c2a3d6d94
Nguyen Duy Thai <92375607+Tox1cCoder@users.noreply.github.com>
2024-11-20 01:35:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+import os
+import time
+from pathlib import Path
+import uuid
+import tempfile
+from typing import Union
+import atexit
+from concurrent.futures import ThreadPoolExecutor
 
+import gradio as gr
+import cv2
+import torch
+import numpy as np
+import click
+import trimesh
+import trimesh.visual
+from PIL import Image
+
+from moge.model import MoGeModel
+from moge.utils.vis import colorize_depth
+import utils3d
